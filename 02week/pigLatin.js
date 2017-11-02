c270f812b726cadec2fc0f5556935a981bc70588
edvidaurri <edward@vidaurrimail.com>
2017-11-02 15:17:38 -0500

    Made changes in pigLatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -13,9 +13,6 @@ const rl = readline.createInterface({
 //If first letter is a vowel just return and add “yay” to the end of word.
 //Find index of first vowel. Create a variable called “firstVowel”.
 
-//Use Regex method “.match” for variable and create a string “aeiou”
-//to search for first occurring vowel in string when function is called.
-
 //Use .indexOf() method to return the first occurrence of item within
 //the array when function is called.
 
@@ -27,24 +24,32 @@ const rl = readline.createInterface({
 const pigLatin = (letter) => {
   //Use .toLowerCase and .trim methods to pass test for Uppercase and spaces.
   letter = letter.toLowerCase().trim();
+//Create an array for vowels.  Run a for loop to iterate the letters in the array
+// then use .indexOf method to find first occurence of the array.
 
-  //Regular Expression method .match will  retrieves the matches when matching
-  // a string against a regular expression.
-  const firstVowel = letter.match(/[aeiou]/);
-
-  //Use .idexOf method to find first occurrence of vowel.
-  const firstLetterPosition = letter.indexOf(firstVowel);
-  //Use if statement to find letter(s) that precede the first occurance of a
-  //vowel and send to back of word and add "ay".
-  if (firstLetterPosition > 0) {
-
-    return letter.slice(firstLetterPosition) + letter.slice(0, firstLetterPosition) + "ay";
-  } else {
-    
-  //Otherwise just add "yay" to end of word that starts with a vowel.
-    return letter + "yay";
-  }
+  const vowelArr = ['a', 'e', 'i', 'o', 'u'];
+ const findFirstVowel = (letter) => {
+   for (var i = 0; i< letter.length; i++) {
+     if(vowelArr.indexOf(letter[i]) !== -1){
+       return i;
+     }
+   }
+ };
+ //Create variable that can be used in an if statement.
+ //Use if statement to find letter(s) that precede the first occurance of a
+ //vowel and send to back of word and add "ay".
+ const firstVowelPosition = findFirstVowel(letter);
+ if (firstVowelPosition > 0){
+   return letter.slice(firstVowelPosition) + letter.slice(0, firstVowelPosition) + "ay";
+ }
+ return letter + "yay";
 }
+
+
+
+    //Otherwise just add "yay" to end of word that starts with a vowel.
+
+
 
 function getPrompt() {
   rl.question('word ', (answer) => {
