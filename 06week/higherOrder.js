a8e873c75ab02cdbfa75040c4d970f7868915b1f
edvidaurri <edward@vidaurrimail.com>
2017-11-09 17:45:56 -0600

    Working on higherOrder

---
+++ 06week/higherOrder.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//Create a forEach() function that takes an array of items and a function that runs the function arr.length number of times.
+
+
+
+// Create a map() function that takes an array of items and a function that returns an array with each item manipulated by that function.
+const test = [1, 2, 3, 4, 5, 6];
+console.log(test.map((num, index) => num +1));
+
+// Create an object and store it to a variable called userObject. It must have at least 8 key/value pairs.
+
+
+
+// Create an array from all of the keys in userObject. Store the array in a variable called userKeyArray.
+
+
+
+// Create an array of all of the values in userObject using userKeyArray and your map() function, store it in a variable called userValueArray.
