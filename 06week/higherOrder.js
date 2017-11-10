00511d2b5004a26566821a145d61f203f7287ebf
edvidaurri <edward@vidaurrimail.com>
2017-11-09 19:39:00 -0600

    More updates

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -1,19 +1,23 @@
 'use strict';
 
 //Create a forEach() function that takes an array of items and a function that runs the function arr.length number of times.
+const dogNames = ['Rocket', 'Tulip', 'Mitsy', 'Bruce', 'Zoe'];
 
+dogNames.forEach((dogNames) => {
+  console.log(`My favorite dog is ${dogNames}`);
+});
 
 
 // Create a map() function that takes an array of items and a function that returns an array with each item manipulated by that function.
 const test = [1, 2, 3, 4, 5, 6];
-console.log(test.map((num, index) => num +1));
-
-// Create an object and store it to a variable called userObject. It must have at least 8 key/value pairs.
+console.log(test.map((num, index) => num *2));
 
 
 
-// Create an array from all of the keys in userObject. Store the array in a variable called userKeyArray.
 
-
-
-// Create an array of all of the values in userObject using userKeyArray and your map() function, store it in a variable called userValueArray.
+//Create a filter() function that takes an array of items and a function that returns an array with only the items that return true in the function.
+const dogNames = ['Rocket', 'Tulip', 'Mitsy', 'Bruce', 'Zoe'];
+  const fiveLetter = dogNames.filter((dogNames) => {
+    return dogNames.length === 5;
+  });
+  console.log(fiveLetter);
