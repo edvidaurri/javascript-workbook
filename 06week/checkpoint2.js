2ca4036e5a18e61f027de73bdcdd7b5f3107ec55
edvidaurri <edward@vidaurrimail.com>
2017-11-11 10:21:28 -0600

    Updating Checkpoint2

--- 06week/checkpoint2.js
+++ 06week/checkpoint2.js
@@ -1,1 +1,47 @@
 'use strict';
+
+let userArray = [
+    {
+            "customer": {
+                "id": 1,
+                "customerName":"Marilyn Monroe",
+                "customerCity":"New York City",
+                "customerState":"NY",
+                "product":"Yellow Chair",
+                "productPrice": 19.99
+            }
+        },
+        {
+            "customer": {
+                "id": 2,
+                "customerName":"Abraham Lincoln",
+                "customerCity":"Boston",
+                "customerState":"MA",
+                "product":"Movie Tickets",
+                "productPrice": 27.00
+            }
+        },
+                {
+            "customer": {
+                "id": 3,
+                "customerName":"John F. Kennedy",
+                "customerCity":"Dallas",
+                "customerState":"TX",
+                "product":"Mustang Convertible",
+                "productPrice": 24999.99
+            }
+        },
+                {
+            "customer": {
+                "id": 4,
+                "customerName":"Martin Luther King",
+                "customerCity":"Burmingham",
+                "customerState":"AL",
+                "product":"Sandwiches",
+                "productPrice": 7.99
+            }
+        },
+];
+const statement = userArray.map(function(){
+  return `${userArray.customerName} paid ${userArray.productPrice} for ${userArray.product} in ${userArray.customerCity}, ${userArray.customerState}.`
+});
