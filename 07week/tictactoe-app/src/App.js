7e9db82a5dfc72f61ff64c5619dbbe7129ebf947
edvidaurri <edward@vidaurrimail.com>
2017-11-22 11:09:42 -0600

    Made some changes

--- 07week/tictactoe-app/src/App.js
+++ 07week/tictactoe-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Square(props) {
+const Square = (props) => {
   return (
     <button className='square' onClick={props.onClick}>
       {props.value}
@@ -34,7 +34,6 @@ class Board extends React.Component {
     value={this.state.squares[i]}
     onClick={() => this.handleClick(i)}
      />
-
   }
 
   render() {
@@ -78,23 +77,18 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          
         </div>
       </div>
     );
   }
 }
-function calculateWinner(squares) {
+const calculateWinner = (squares) => {
   const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
+
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
