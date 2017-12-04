a29b378254bcce94c47151835fa532f23376d2fa
edvidaurri <edward@vidaurrimail.com>
2017-12-03 20:25:25 -0600

    Added material-ui

--- 08week/StarWars/starswars/src/App.js
+++ 08week/StarWars/starswars/src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+
 class App extends Component {
  state = {
      characters: null
@@ -17,6 +25,14 @@ class App extends Component {
             console.error(error);
         });
  }
+ constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
  renderCharacters(){
      const { characters } = this.state;
      if(characters && characters.length > 0){
@@ -33,6 +49,26 @@ class App extends Component {
           <h1 className="App-title">Pokemon</h1>
         </header>
           {this.renderCharacters()}
+      </div>
+      <MuiThemeProvider>
+        <TextFieldExampleSimple />
+        <IconMenuExampleSimple/>
+        <RaisedButtonExampleSimple/>
+      </MuiThemeProvider>
+      <div>
+        <RaisedButton
+          label="Open Drawer"
+          onClick={this.handleToggle}
+        />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
     );
   }
