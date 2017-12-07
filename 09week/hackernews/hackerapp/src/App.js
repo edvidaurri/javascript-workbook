7f87fe1c9b16eec7fa8abc2287d63c98ff27d666
edvidaurri <edward@vidaurrimail.com>
2017-12-06 21:14:56 -0600

    Used fetch

--- 09week/hackernews/hackerapp/src/App.js
+++ 09week/hackernews/hackerapp/src/App.js
@@ -2,62 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-state = {
-stories: [],
+  state = {
+  stories: [],
 }
 
 componentDidMount(){
-
-fetch
-this.setState({stories: [...this.state.stories, story]})
+  fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then((res) => {
+  res.json().then((storyIds) => {
+    storyIds.forEach((storyId) => {
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`).then((res) => {
+        res.json().then((story) => {
+          console.log(story);
+        })
+      });
+    });
+  });
+});
+  // this.setState({stories: [...this.state.stories, story]})
 // const newState = this.state.storires;
 // newState.push(story);
 // this.setState({stories: newState})
 }
-renderStories(){
-return this.state.stories.map((story, key) => {
-return <li key={key}>{story.title}</li>
-})
+  renderStories(){
+  return this.state.stories.map((story, key) => {
+  return <li key={key}>{story.title}</li>
+  })
 }
 render() {
+  return (
+    <div>
 
-return (
-<div>
-{this.renderStories()}
-</div>
-);
-}
-}
-
-export default App;
-import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-state = {
-stories: [],
-}
-
-componentDidMount(){
-fetch
-this.setState({stories: [...this.state.stories, story]})
-// const newState = this.state.storires;
-// newState.push(story);
-// this.setState({stories: newState})
-}
-renderStories(){
-return this.state.stories.map((story, key) => {
-return <li key={key}>{story.title}</li>
-})
-}
-render() {
-
-return (
-<div>
-{this.renderStories()}
-</div>
-);
-}
+    {this.renderStories()}
+    </div>
+    );
+  }
 }
 
 export default App;
