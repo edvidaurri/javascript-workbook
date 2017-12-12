751ad18ccc09b74fff774ce3e27a5e78b4c0b29b
edvidaurri <edward@vidaurrimail.com>
2017-12-11 20:23:09 -0600

    Trying to fix error

--- 09week/hackernews/hackerapp/src/App.js
+++ 09week/hackernews/hackerapp/src/App.js
@@ -3,29 +3,36 @@ import './App.css';
 
 class App extends Component {
   state = {
-  stories: [],
+    stories: [],
 }
 
+
+
 componentDidMount(){
-  fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then((res) => {
-  res.json().then((storyIds) => {
-    storyIds.forEach((storyId) => {
-      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`).then((res) => {
-        res.json().then((story) => {
-          console.log(story);
-        })
-      });
+  fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    .then(res => res.json())
+    .then(storyIds => {
+  storyIds.slice(0, 15).forEach((storyId) => {
+    fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`)
+    .then(res => res.json())
+    .then(story => {
+console.log(story);
+      })
     });
   });
-});
-  // this.setState({stories: [...this.state.stories, story]})
+}
+
+// Syntax error: Unexpected token (27:4)
+
+// this.setState({stories: [...this.state.stories, story]})
 // const newState = this.state.storires;
 // newState.push(story);
 // this.setState({stories: newState})
-}
-  renderStories(){
+
+
+renderStories(){
   return this.state.stories.map((story, key) => {
-  return <li key={key}>{story.title}</li>
+    return <li key={key}>{story.title}</li>
   })
 }
 render() {
