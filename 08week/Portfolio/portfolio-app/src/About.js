d502be16d543ab264c60a4bc6872659e6a7a8f5e
edvidaurri <edward@vidaurrimail.com>
2017-12-15 16:01:39 -0600

    more updates

---
+++ 08week/Portfolio/portfolio-app/src/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About Me</h2>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default About;
