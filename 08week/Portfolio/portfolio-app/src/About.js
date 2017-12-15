43918fd32e24d88191c0156ac6b8c9038ae6ab00
edvidaurri <edward@vidaurrimail.com>
2017-12-15 15:26:06 -0600

    Updated file

--- 08week/Portfolio/portfolio-app/src/About.js
+++ 08week/Portfolio/portfolio-app/src/About.js
@@ -5,7 +5,7 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h2>About</h2>
+        <h2>About Me</h2>
       </div>
     );
   }
--- 08week/Portfolio/portfolio-app/src/App.js
+++ 08week/Portfolio/portfolio-app/src/App.js
@@ -1,89 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
 import {
   BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom'
+} from 'react-router-dom';
+import Start from './Start';
+import Images from './Images';
+import Judge from './Judge';
+import Winner from './Winner';
 
 import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-import IconMenu from 'material-ui/IconMenu';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
-import IconButton from 'material-ui/IconButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import About from './About';
-import Home from './Home';
 
-//Hamburger Menu
-
-//Text field where name label moves to top.const TextFieldExampleSimple = () => (
-
-
-
-//Raised buttons.
-  const style = {
-  margin: 12,
-  };
-
-
-
-  //Destructuring JavaScript Objects.
-  const person = {
-first: 'Eddie',
-last: 'Vidaurri',
-country: 'USA',
-city: 'Austin',
+const style = {
+  marginLeft: 100
 };
 
-
-const { first, last, country, city } = person;
-console.log(first, city, country, last);
-
 class App extends Component {
+  state = {
+    open: false,
+  };
+
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
   render() {
     return (
       <Router>
-      <div>
-        <IconMenu
-          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-          anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-        >
-          <MenuItem primaryText="Send feedback" animated/>
-          <MenuItem primaryText="Settings" />
-          <MenuItem primaryText="Help" />
-          <MenuItem primaryText="Sign out" />
-        </IconMenu>
-
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-
-        </ul>
-
-        <hr/>
-
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
         <div>
-          <TextField
-          floatingLabelText="Name"
-          />
+          <Link to="/"><RaisedButton onClick={this.handleTouchTap} label="Pick Meme!" fullWidth={true} primary={true} /></Link>
+          <Popover
+            style={style}
+            open={this.state.open}
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            onRequestClose={this.handleRequestClose}
+          >
+            <Menu>
+              <MenuItem><Link to="/">Landing Page</Link></MenuItem>
+              <MenuItem><Link to="/Start.js"><RaisedButton label="Who is playing?" fullWidth={true} primary={true} /></Link></MenuItem>
+              <MenuItem><Link to="/Images.js"><RaisedButton label="Choose a caption!" fullWidth={true} primary={true} /></Link></MenuItem>
+              <MenuItem><Link to="/Judge.js"><RaisedButton label="Pick a winner!" fullWidth={true} primary={true} /></Link></MenuItem>
+              <MenuItem><Link to="/Winner.js"><RaisedButton label="Winner!" fullWidth={true} primary={true} /></Link></MenuItem>
+            </Menu>
+          </Popover>
+
+          <hr/>
+          <Route exact path="/" component={App}/>
+          <Route exact path="/Start.js" component={Start}/>
+          <Route path="/Images.js" component={Images}/>
+          <Route path="/Judge.js" component={Judge}/>
+          <Route path="/Winner.js" component={Winner}/>
         </div>
-        <div>
-          <RaisedButton label="Primary" primary={true} />
-          <RaisedButton label="Secondary" secondary={true}style={style} />
-
-          <br/><br/><br/>
-          <RaisedButton label="Full width" fullWidth={true} />
-        </div>
-      </div>
-    </Router>
-
-
+      </Router>
     );
   }
 }
